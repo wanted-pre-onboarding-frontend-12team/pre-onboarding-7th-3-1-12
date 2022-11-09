bf29d852b9ad5b72e9398306d3ea6a9078b9219e
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-09 22:42:26 +0900

    :lipstick: GlobalStyle  theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+	fontWeights: {
+		normal: 400,
+		medium: 500,
+		bold: 700,
+	},
+	colors: {
+		sky: '#CAE9FF',
+		blue: '#017BE8',
+		black: '#000000',
+		white: '#FFFFFF',
+	},
+} as const;
+
+export default theme;
