70a83872f9e750f0fbf53a66974f4a417bb9aad6
hopak-e <a328028@gmail.com>
2022-11-11 01:44:52 +0900

    ✨ 전역으로 관리하는 theme, global style 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle, theme } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
 	</React.StrictMode>,
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+const theme = {
+	fontSizes: {
+		list: '16px',
+		search: '18px',
+		title: '34px',
+	},
+	fontWeights: {
+		fw400: 400,
+		fw500: 500,
+		fw700: 700,
+	},
+	colors: {
+		blue: {
+			'100': '#EEF8FF',
+			'200': '#CAE9FF',
+			'300': '#67B1F2',
+			'400': '#007BE9',
+		},
+		gray: {
+			'100': '#C9CCCF',
+			'200': '#A7AFB7',
+		},
+		black: '#000000',
+		white: '#FFFFFF',
+	},
+} as const;
+
+export default theme;
