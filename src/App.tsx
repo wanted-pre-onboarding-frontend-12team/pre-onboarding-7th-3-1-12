4e7d1f7935c831a977d802429f3938d52bf92107
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-11 04:14:48 +0900

    :lipstick: 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Layout from './components/layout';
 import Router from './router';
 
 function App() {
-	return <Router />;
+	return (
+		<Layout>
+			<Router />
+		</Layout>
+	);
 }
 
 export default App;
