44a0e3b08d267fde1577642fc51bdaf797b1c0be
hopak-e <a328028@gmail.com>
2022-11-11 01:46:51 +0900

    📝 파일들 index.ts에 묶어 한 파일에서 import 할 수 있게 조정

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as searchIconLarge } from './searchIconLarge.png';
+export { default as searchIconSmall } from './searchIconSmall.png';
---
+++ src/constants/api.ts
@@ -0,0 +1,5 @@
+export const PATH = {
+	sick: {
+		index: '/sick',
+	},
+};
